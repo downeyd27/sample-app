3180f9a047e86acf9ecf9a301bc2fed92dc86d44
Dillon Downey <downeydillon@gmail.com>
2015-07-18 12:39:00 -0700

    6 of 6 passing tests for title and h1 tag for about, home, and help static_pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe "StaticPages", type: :feature do
 
     it "should have the title 'Home'" do
       visit '/static_pages/home'
-      expect(page).to have_selector('title', text: 'Home')
+      expect(page).to have_title 'Home'
     end
 
     it "should have the h1 'Home'" do
@@ -18,14 +18,14 @@ RSpec.describe "StaticPages", type: :feature do
 
   describe "Help Page" do
 
+    it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_title 'Help'
+    end
+
     it "should have the h1 'Help'" do
       visit '/static_pages/help'
       expect(page).to have_selector('h1', text: 'Help')
-    end
-
-    it "should have the title 'Help'" do
-      visit '/static_pages/help'
-      expect(page).to have_selector('title', text: 'Help')
     end
   end
 
@@ -38,7 +38,7 @@ RSpec.describe "StaticPages", type: :feature do
 
     it "should have the title 'About Us'" do
       visit '/static_pages/about'
-      expect(page).to have_selector('title', text: 'About Us')
+      expect(page).to have_title 'About Us'
     end
   end
 end
