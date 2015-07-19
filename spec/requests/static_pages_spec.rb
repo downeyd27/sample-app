01033a27bbdc1b3565021afc007ef6da1e081d81
Dillon Downey <downeydillon@gmail.com>
2015-07-19 14:27:40 -0700

    DRY up the visit paths for the spec file

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,52 +2,46 @@ require 'rails_helper'
 
 RSpec.describe "StaticPages Controller", type: :feature do
   describe "Home Page" do
+    before { visit root_path }
 
     it "should have the title 'Home'" do
-      visit root_path
       expect(page).to have_title 'Home'
     end
 
     it "should have the h1 'Home'" do
-      visit root_path
       expect(page).to have_selector('h1', text: 'Home Page')
     end
   end
 
   describe "Help Page" do
-
+    before { visit help_path }
     it "should have the title 'Help'" do
-      visit help_path
       expect(page).to have_title 'Help'
     end
 
     it "should have the h1 'Help'" do
-      visit help_path
       expect(page).to have_selector('h1', text: 'Help')
     end
   end
 
   describe "About Page" do
-
+    before { visit about_path }
     it "should have the h1 'About Us'" do
-      visit about_path
       expect(page).to have_selector('h1', text: 'About Us')
     end
 
     it "should have the title 'About Us'" do
-      visit about_path
       expect(page).to have_title 'About Us'
     end
   end
 
   describe "Contact Page" do
+    before { visit contact_path }
     it "should have the h1 'Contact'" do
-      visit contact_path
       expect(page).to have_selector('h1', text: 'Contact')
     end
 
     it "should the title 'Contact'" do
-      visit contact_path
       expect(page).to have_title 'Contact'
     end
   end
