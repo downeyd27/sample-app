59612109325efa2d948ed32e1a3630132bb2cee6
Dillon Downey <downeydillon@gmail.com>
2015-07-19 13:38:53 -0700

    Change specs to use rails path names

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,12 +4,12 @@ RSpec.describe "StaticPages Controller", type: :feature do
   describe "Home Page" do
 
     it "should have the title 'Home'" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).to have_title 'Home'
     end
 
     it "should have the h1 'Home'" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).to have_selector('h1', text: 'Home Page')
     end
   end
@@ -17,12 +17,12 @@ RSpec.describe "StaticPages Controller", type: :feature do
   describe "Help Page" do
 
     it "should have the title 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_title 'Help'
     end
 
     it "should have the h1 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_selector('h1', text: 'Help')
     end
   end
@@ -30,24 +30,24 @@ RSpec.describe "StaticPages Controller", type: :feature do
   describe "About Page" do
 
     it "should have the h1 'About Us'" do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_selector('h1', text: 'About Us')
     end
 
     it "should have the title 'About Us'" do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_title 'About Us'
     end
   end
 
   describe "Contact Page" do
     it "should have the h1 'Contact'" do
-      visit '/static_pages/contact'
+      visit contact_path
       expect(page).to have_selector('h1', text: 'Contact')
     end
 
     it "should the title 'Contact'" do
-      visit '/static_pages/contact'
+      visit contact_path
       expect(page).to have_title 'Contact'
     end
   end
