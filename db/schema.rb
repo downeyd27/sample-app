f66c650ce3c7a13e2e3c437a7c35e7ed781f2829
Dillon Downey <downeydillon@gmail.com>
2015-07-20 15:38:44 -0700

    Migrate index to email, password_digest, password, and password_confirmation to user table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150720195930) do
+ActiveRecord::Schema.define(version: 20150720210934) do
 
   create_table "add_index_to_users_emails", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -21,9 +21,11 @@ ActiveRecord::Schema.define(version: 20150720195930) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",            null: false
+    t.datetime "updated_at",            null: false
     t.string   "password_digest"
+    t.string   "password"
+    t.string   "password_confirmation"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
