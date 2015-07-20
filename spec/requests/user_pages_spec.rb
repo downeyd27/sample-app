360747efcb2eeb6054f227742435036376187a79
Dillon Downey <downeydillon@gmail.com>
2015-07-19 17:35:58 -0700

    Add specs for h1 and title

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,10 +1,13 @@
 require 'rails_helper'
 
-RSpec.describe "UserPages", type: :request do
-  describe "GET /user_pages" do
-    it "works! (now write some real specs)" do
-      get user_pages_index_path
-      expect(response).to have_http_status(200)
+RSpec.describe "UserPages", type: :feature do
+  describe "User Pages" do
+
+    describe "Sign-up Page" do
+      before { visit signup_path }
+
+      it { expect(page).to have_selector('h1', text: 'Sign up')}
+      it { expect(page).to have_title 'Sign up'}
     end
   end
 end
