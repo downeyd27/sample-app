27e36b545030e4a2a224a4f956733c2a52068764
Dillon Downey <downeydillon@gmail.com>
2015-07-20 15:42:27 -0700

    Add has_secure_password method and password validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,9 +11,18 @@
 
 class User < ActiveRecord::Base
   # downcase email before saving because not all database adapters use case-sensitive indices
+  has_secure_password
+
   before_save { |user| user.email = email.downcase }
+
   validates :name, presence: true, length: { maximum: 50 }
+
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  validates :email, presence: true,
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :password_confirmation, presence: true
 
 end
