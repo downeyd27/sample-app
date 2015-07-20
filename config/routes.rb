73b565f447281702b2309c76f6785d8ec36279d6
Dillon Downey <downeydillon@gmail.com>
2015-07-19 17:55:47 -0700

    Add match to '/'. Fixes hashtag that comes after root route in URL. This Fixes signup_path link

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
+  match '/', to: 'static_pages#home', via: :get
   # Static Pages Controller
   match '/help' , to: 'static_pages#help', via: :get
   match '/about' , to: 'static_pages#about', via: :get
