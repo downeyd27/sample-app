4a0f073b9a52cc48ec3a548fcd26deb0b53f5d0c
Dillon Downey <downeydillon@gmail.com>
2015-07-20 11:17:13 -0700

    Add test for valid email and if email is already taken

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -35,7 +35,7 @@ RSpec.describe User, type: :model do
   end
 
   describe "when email format is invalid" do
-    it "email should be invalid" do
+    it "should be invalid" do
       addresses = %w[user@foo,com user_at_foo.org example.user@foo. foo@bar_baz.com foo@bar+baz.com]
       addresses.each do |invalid_address|
         @user.email = invalid_address
@@ -45,12 +45,22 @@ RSpec.describe User, type: :model do
   end
 
   describe "when email format is valid" do
-     it "should be valid" do
-       addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
-       addresses.each do |valid_address|
-         @user.email = valid_address
-         expect(@user).to be_valid
-       end
-     end
-   end
+    it "should be valid" do
+      addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
+      addresses.each do |valid_address|
+        @user.email = valid_address
+        expect(@user).to be_valid
+      end
+    end
+  end
+
+  describe "when email address is already taken" do
+    before do
+      user_with_same_email = @user.dup
+      user_with_same_email.email = @user.email.upcase
+      user_with_same_email.save
+    end
+
+    it { expect(@user).to_not be_valid }
+  end
 end
