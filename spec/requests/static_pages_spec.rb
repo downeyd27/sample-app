8dda0dddd8e733a1ad9cf1922fada518c15a5d2a
Dillon Downey <downeydillon@gmail.com>
2015-07-19 18:28:47 -0700

    Refactor static pages tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,32 +1,42 @@
 require 'rails_helper'
 
-RSpec.describe "StaticPages Controller", type: :feature do
+RSpec.describe "Static Pages", type: :feature do
+
+  shared_examples_for "all static pages" do
+    it { expect(page).to have_title full_title(page_title) }
+    it { expect(page).to have_selector('h1', text: heading) }
+  end
 
   describe "Home Page" do
     before { visit root_path }
+    let(:heading) { 'Home Page' }
+    let(:page_title) { '' }
 
-    it { expect(page).to have_title full_title('')}
-    it { expect(page).to have_selector('h1', text: 'Home Page') }
+    it_should_behave_like 'all static pages'
+    it { expect(page).to_not have_title '| Home'}
   end
 
   describe "Help Page" do
     before { visit help_path }
+    let(:heading) { 'Help Page' }
+    let(:page_title) { 'Help' }
 
-     it { expect(page).to have_title full_title('Help') }
-     it { expect(page).to have_selector('h1', text: 'Help')}
+    it_should_behave_like 'all static pages'
   end
 
   describe "About Page" do
     before { visit about_path }
+    let(:heading) { 'About Us' }
+    let(:page_title) { 'About Us' }
 
-    it { expect(page).to have_title full_title('About Us') }
-    it { expect(page).to have_selector('h1', text: 'About Us')}
+    it_should_behave_like 'all static pages'
   end
 
   describe "Contact Page" do
     before { visit contact_path }
+    let(:heading) { 'Contact' }
+    let(:page_title) { 'Contact' }
 
-    it { expect(page).to have_title full_title('Contact') }
-    it { expect(page).to have_selector('h1', text: 'Contact')}
+    it_should_behave_like 'all static pages'
   end
 end
