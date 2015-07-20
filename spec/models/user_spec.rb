1cd4782051664593aa8b3343550aeaaab0d19af6
Dillon Downey <downeydillon@gmail.com>
2015-07-20 15:52:29 -0700

    Add test that before_save callback is downcasing email

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -72,6 +72,16 @@ RSpec.describe User, type: :model do
     it { expect(@user).to_not be_valid }
   end
 
+  describe "email address with mixed case" do
+    let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      expect(@user.reload.email) == mixed_case_email.downcase
+    end
+  end
+
   describe "password is not present" do
     before { @user.password = @user.password_confirmation = " "}
     it { expect(@user).to_not be_valid }
