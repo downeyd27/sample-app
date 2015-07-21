20fe0e81074409ef41f8a09032895c86cd4b8db6
Dillon Downey <downeydillon@gmail.com>
2015-07-20 18:28:30 -0700

    Add sign up page tests

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -2,8 +2,28 @@ require 'rails_helper'
 
 RSpec.describe "User pages", type: :feature do
 
-  describe "sign-up page" do
+  describe "signup page" do
     before { visit signup_path }
+    let(:submit) { "Create my account"}
+
+    describe "with invalid information" do
+      it "should not create user" do
+        expect { click_button submit }.not_to change(User, :count)
+      end
+    end
+
+    describe "with valid information" do
+      before do
+        fill_in "Name", with: "Test User"
+        fill_in "email", with: "test@user.com"
+        fill_in "Password", with: "foobar"
+        fill_in "Password_confirmation", with: "foobar"
+      end
+
+      it "should create user" do
+        expect { click_button submit }.to change(User, :count).by(1)
+      end
+    end
 
     it { expect(page).to have_title 'Sign Up'}
     it { expect(page).to have_selector('h1', text: 'Sign Up')}
