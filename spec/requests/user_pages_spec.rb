9159815235f30f241a38369fbcaf317553dbc4ba
Dillon Downey <downeydillon@gmail.com>
2015-07-20 17:01:08 -0700

    Add tests for profile page to have h1 and title

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,20 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe "User pages", type: :feature do
-  describe "User Pages" do
 
-    describe "sign-up page" do
-      before { visit signup_path }
+  describe "sign-up page" do
+    before { visit signup_path }
 
-      it { expect(page).to have_title 'Sign Up'}
-      it { expect(page).to have_selector('h1', text: 'Sign Up')}
-    end
+    it { expect(page).to have_title 'Sign Up'}
+    it { expect(page).to have_selector('h1', text: 'Sign Up')}
+  end
 
-    describe "profile page" do
-      before { visit user_path(user) }
+  describe "profile page" do
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit user_path(user) }
 
-      it { expect(page).to have_title text: 'user.name' }
-      it { expect(page).to have_selector('h1', text: 'user.name') }
-    end
+    it { expect(page).to have_title user.name }
+    it { expect(page).to have_selector('h1', text: 'user.name') }
   end
 end
