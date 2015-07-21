7148b9a49dfaebdee55d19b5278f3fb13aad8226
Dillon Downey <downeydillon@gmail.com>
2015-07-21 16:16:36 -0700

    Remove tests for user pages that belong in spec/features folder

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,54 +1,29 @@
 require 'rails_helper'
 
-RSpec.describe "User pages", type: :feature do
+RSpec.describe "UserPages", type: :request do
 
-  describe "signup page" do
-    before { visit signup_path }
-    let(:submit) { "Create my account"}
+  describe "GET new" do
+    before { get new_user_path }
 
-    describe "with invalid information" do
-      it "should not create user" do
-        expect { click_button submit }.not_to change(User, :count)
-      end
-
-      describe "after submission" do
-        before { click_button submit }
-
-        it { expect(page).to have_title 'Sign Up' }
-        it { expect(page).to have_content('error') }
-      end
+    it "has http status responds 200(OK)" do
+      expect(response).to have_http_status(200)
     end
 
-    describe "with valid information" do
-      before do
-        fill_in "Name", with: "Test User"
-        fill_in "Email", with: "user@example.com"
-        fill_in "Password", with: "foobar"
-        fill_in "Password Confirmation", with: "foobar"
-      end
-
-      it "should create user" do
-        expect { click_button submit }.to change(User, :count).by(1)
-      end
-
-      describe "after saving the user" do
-        before { click_button submit }
-        let(:user) { User.find_by_email('user@example.com') }
-
-        it { expect(page).to have_title user.name }
-        it { expect(page).to have_selector('div.alert.alert-success', text: 'Welcome') }
-      end
+    it "renders user new (sign up) page view" do
+      expect(response).to render_template(:new)
     end
-
-    it { expect(page).to have_title 'Sign Up'}
-    it { expect(page).to have_selector('h1', text: 'Sign Up')}
   end
 
-  describe "profile page" do
+  describe "GET show" do
     let(:user) { FactoryGirl.create(:user) }
-    before { visit user_path(user) }
+    before { get user_path(user) }
 
-    it { expect(page).to have_title user.name }
-    it { expect(page).to have_selector('h1', text: user.name) }
+    it "has http status responds 200(OK)" do
+      expect(response).to have_http_status(200)
+    end
+
+    it "renders show page view" do
+      expect(response).to render_template(:show)
+    end
   end
 end
