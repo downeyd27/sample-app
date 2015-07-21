81fa703d50f2dd663d91893a507933980a7dddad
Dillon Downey <downeydillon@gmail.com>
2015-07-21 16:15:15 -0700

    Add tests for https status 200 and rendering template for static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,70 +1,52 @@
 require 'rails_helper'
 
-RSpec.describe "Static Pages", type: :feature do
+RSpec.describe "StaticPages", type: :request do
 
-  it "should have the right links on the layout" do
-    visit root_path
-    click_link "Help"
-    expect(page).to have_title full_title('Help')
+  describe "GET home" do
+    before { get root_path }
 
-    within("header") do
-      click_link "About"
-      expect(page).to have_title full_title('About')
+    it "has http status responds 200(OK)" do
+      expect(response).to have_http_status(200)
     end
-    within("footer") do
-      click_link "About"
-      expect(page).to have_title full_title('About')
+
+    it "renders home page view" do
+      expect(response).to render_template(:home)
     end
-    within("header") do
-      click_link "Contact"
-      expect(page).to have_title full_title('Contact')
+  end
+
+  describe "GET help" do
+    before { get help_path }
+
+    it "has http status responds 200(OK)" do
+      expect(response).to have_http_status(200)
     end
-    within("footer") do
-      click_link "Contact"
-      expect(page).to have_title full_title('Contact')
+
+    it "renders help page view" do
+      expect(response).to render_template(:help)
     end
-    click_link "Home"
-    click_link "Sign Up"
-    expect(page).to have_title full_title('Sign Up')
-    click_link "logo"
-    expect(page).to have_title full_title('')
   end
 
-  shared_examples_for "all static pages" do
-    it { expect(page).to have_title full_title(page_title) }
-    it { expect(page).to have_selector('h1', text: heading) }
+  describe "GET about" do
+    before { get about_path }
+
+    it "has http status responds 200(OK)" do
+      expect(response).to have_http_status(200)
+    end
+
+    it "renders about page view" do
+      expect(response).to render_template(:about)
+    end
   end
 
-  describe "Home Page" do
-    before { visit root_path }
-    let(:heading) { 'Home Page' }
-    let(:page_title) { '' }
+  describe "GET contact" do
+    before { get contact_path }
 
-    it_should_behave_like 'all static pages'
-    it { expect(page).to_not have_title '| Home'}
-  end
+    it "has http status responds 200(OK)" do
+      expect(response).to have_http_status(200)
+    end
 
-  describe "Help Page" do
-    before { visit help_path }
-    let(:heading) { 'Help Page' }
-    let(:page_title) { 'Help' }
-
-    it_should_behave_like 'all static pages'
-  end
-
-  describe "About Page" do
-    before { visit about_path }
-    let(:heading) { 'About' }
-    let(:page_title) { 'About' }
-
-    it_should_behave_like 'all static pages'
-  end
-
-  describe "Contact Page" do
-    before { visit contact_path }
-    let(:heading) { 'Contact' }
-    let(:page_title) { 'Contact' }
-
-    it_should_behave_like 'all static pages'
+    it "renders contact page view" do
+      expect(response).to render_template(:contact)
+    end
   end
 end
