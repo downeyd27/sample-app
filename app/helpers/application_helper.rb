7b7a14b8c57fa41768d31502fbe2a9282831c73a
Dillon Downey <downeydillon@gmail.com>
2015-07-20 21:00:37 -0700

    Refactor base_title to be able to use in Users Controller

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,15 @@
 module ApplicationHelper
   # Returns the full title on a per-page basis
+  def base_title
+    "Project Name"
+  end
+
   def full_title(page_title)
-    base_title = "Project Name"
     if page_title.empty?
       base_title
     else
       "#{base_title} | #{page_title}"
     end
   end
+
 end
