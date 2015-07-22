df14ebf4681121287de336626466dea249819541
Dillon Downey <downeydillon@gmail.com>
2015-07-22 15:42:57 -0700

    Remove destroy method from helper and place in sessions controller

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -18,11 +18,6 @@ module SessionsHelper
     @current_user ||= User.find_by_remember_token(cookies[:remember_token])
   end
 
-  def destroy
-    sign_out
-    redirect_to root_url
-  end
-
   def sign_out
     self.current_user = nil
     cookies.delete(:remember_token)
