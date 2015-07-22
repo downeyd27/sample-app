3495ca2216413293a061a1ec1f2d4e6e654af5d9
Dillon Downey <downeydillon@gmail.com>
2015-07-21 17:38:39 -0700

    Add tests to check if invalid signin banner persists after visiting another page with invalid sign in info is given

--- spec/features/authenticaation_pages_spec.rb
+++ spec/features/authenticaation_pages_spec.rb
@@ -5,27 +5,34 @@ RSpec.describe "AuthenticationPages", type: :feature do
   describe 'signin page' do
     before { visit signin_path }
 
-    describe "with invalid information" do
-      before { click_button 'Sign in' }
-
-      it { expect(page).to have_selector('div.alert.alert-error', text: 'Invalid') }
-    end
-
-    describe "with valid information" do
-      before do
-        let(:user) { FactoryGirl.create(:user) }
-        fill_in "Email",    with: user.email.upcase # upcase so that we make sure use in database is case-insensitive
-        fill_in "password", with: user.password
-        click_button 'Sign in'
-      end
-
-      it { expect(page).to have_selector('div.alert.alert-success', text: 'Valid') }
-    end
-
     it { expect(page).to have_title 'Sign in' }
     it { expect(page).to have_selector( 'h1', text: 'Sign in') }
     it { expect(page).to have_link 'Profile', href: user_path(user) }
     it { expect(page).to have_link 'Sign out', href: signout_path }
     it { expect(page).to_not have_link 'Sign in', href: signin_path }
+
+    describe "with invalid information" do
+      before { click_button 'Sign in' }
+
+      it { expect(page).to have_selector('div.alert.alert-error', text: 'Invalid') }
+
+      describe "after visiting another page" do
+        before { click_link 'Home' }
+
+        it { expect(page).to_not have_selector('div.alert.alert-error')}
+      end
+    end
+
+
+    describe "with valid information" do
+      let(:user) { FactoryGirl.create(:user) }
+      before do
+        fill_in "Email",    with: user.email.upcase # upcase so that we make sure use in database is case-insensitive
+        fill_in "Password", with: user.password
+        click_button 'Sign in'
+      end
+
+      it { expect(page).to have_selector('div.alert.alert-success', text: 'Valid') }
+    end
   end
 end
