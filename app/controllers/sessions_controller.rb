35fa981dd5866f8c859cd6d0443fd34e6bfde6e3
Dillon Downey <downeydillon@gmail.com>
2015-07-21 17:45:39 -0700

    Add sign_in method for user for future

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by_email(params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
+      sign_in user
       status = 201
       flash[:success] = "Valid signin credentials.  You just signed in!"
       redirect_to user_path(user)
