9940cb00084d67b7018ddaeb1873b049134b95fa
Dillon Downey <downeydillon@gmail.com>
2015-07-22 20:52:23 -0700

    Tests are passing.

--- spec/features/user_pages_spec.rb
+++ spec/features/user_pages_spec.rb
@@ -1,54 +1,74 @@
 require 'rails_helper'
 
-RSpec.describe "UserPages", type: :feature do
+RSpec.describe "User pages", type: :feature do
+  let(:user) { FactoryGirl.create(:user) }
 
-  describe "signup page" do
+  describe "signup" do
     before { visit signup_path }
     let(:submit) { "Create my account"}
 
-    describe "with invalid information" do
-      it "should not create user" do
-        expect { click_button submit }.not_to change(User, :count)
+    describe "page" do
+      describe "with invalid information" do
+        it "should not create user" do
+          expect { click_button submit }.not_to change(User, :count)
+        end
+
+        describe "after submission" do
+          before { click_button submit }
+
+          it { expect(page).to have_title 'Sign Up' }
+          it { expect(page).to have_content('error') }
+        end
       end
 
-      describe "after submission" do
-        before { click_button submit }
+      describe "with valid information" do
+        before do
+          fill_in "Name", with: "Test User"
+          fill_in "Email", with: "user@example.com"
+          fill_in "Password", with: "foobar"
+          fill_in "Confirm Password", with: "foobar"
+        end
 
-        it { expect(page).to have_title 'Sign Up' }
-        it { expect(page).to have_content('error') }
+        it "should create user" do
+          expect { click_button submit }.to change(User, :count).by(1)
+        end
+
+        describe "after saving the user" do
+          before { click_button submit }
+          let(:user) { User.find_by_email('user@example.com') }
+
+          it { expect(page).to have_title user.name }
+          it { expect(page).to have_selector('div.alert.alert-success', text: 'Welcome') }
+        end
       end
+
+      it { expect(page).to have_title 'Sign Up'}
+      it { expect(page).to have_selector('h1', text: 'Sign Up')}
     end
-
-    describe "with valid information" do
-      before do
-        fill_in "Name", with: "Test User"
-        fill_in "Email", with: "user@example.com"
-        fill_in "Password", with: "foobar"
-        fill_in "Password Confirmation", with: "foobar"
-      end
-
-      it "should create user" do
-        expect { click_button submit }.to change(User, :count).by(1)
-      end
-
-      describe "after saving the user" do
-        before { click_button submit }
-        let(:user) { User.find_by_email('user@example.com') }
-
-        it { expect(page).to have_title user.name }
-        it { expect(page).to have_selector('div.alert.alert-success', text: 'Welcome') }
-      end
-    end
-
-    it { expect(page).to have_title 'Sign Up'}
-    it { expect(page).to have_selector('h1', text: 'Sign Up')}
   end
 
-  describe "profile page" do
-    let(:user) { FactoryGirl.create(:user) }
+  describe "profile" do
     before { visit user_path(user) }
 
-    it { expect(page).to have_title user.name }
-    it { expect(page).to have_selector('h1', text: user.name) }
+    describe "page" do
+      it { expect(page).to have_title user.name }
+      it { expect(page).to have_selector('h1', text: user.name) }
+    end
+  end
+
+  describe "edit" do
+    before { visit edit_user_path(user) }
+
+    describe "page" do
+      it { expect(page).to have_title 'Edit user' }
+      it { expect(page).to have_selector('h1', text: 'Update your profile') }
+      it { expect(page).to have_link('change', href: 'https://gravatar.com/emails') }
+    end
+
+    describe "with invalid information" do
+      before { click_button 'Save Changes' }
+
+      it { expect(page).to have_content 'error' }
+    end
   end
 end
