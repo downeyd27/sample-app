e9a9fd117143e8575ec12e49f4adad4e0c36e21b
Dillon Downey <downeydillon@gmail.com>
2015-07-23 14:54:18 -0700

    Add specing for clarity.

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -6,11 +6,13 @@ def sign_in(user)
   fill_in 'Email', with: user.email
   fill_in 'Password', with: user.password
   click_button 'Sign in'
+  #
   # Sign in when not using Capybara as well.
   #
   # COMMENT COOKIES BACK IN BEFORE PRODUCTION AND DEVELOPEMENT.
   #
   # Test suite doesn't recognize method cookie.
+  #
   # Comment out during testing
   # cookies[:remember_token] = user.remember_token
 end
