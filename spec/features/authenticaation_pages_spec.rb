b76af999bf850f00297193e8d5e4cb15a45ab501
Dillon Downey <downeydillon@gmail.com>
2015-07-23 10:50:21 -0700

    Add authorization tests

--- spec/features/authenticaation_pages_spec.rb
+++ spec/features/authenticaation_pages_spec.rb
@@ -46,5 +46,25 @@ RSpec.describe "Authentication", type: :feature do
         it { expect(page).to_not have_selector('div.alert.alert-error') }
       end
     end
+
+    describe "authorization" do
+
+      describe "for non-sign-in users" do
+
+        describe "in the Users controller" do
+
+          describe "visiting the edit page" do
+            before { visit edit_user_path(user) }
+            it { expect(page).to have_title 'Sign in' }
+          end
+
+          describe "submitting to the update action" do
+            # undefined method put error
+            before { put user_path(user) }
+            it { expect(response).to redirect_to(signin_path) }
+          end
+        end
+      end
+    end
   end
 end
