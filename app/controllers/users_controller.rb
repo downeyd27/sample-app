4b8b6839396ecbaa62e5225acbbdd4e04e769ffe
Dillon Downey <downeydillon@gmail.com>
2015-07-22 22:09:32 -0700

    Indent user_params method because its a private method.  Easy identifier of private methods.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -45,8 +45,9 @@ class UsersController < ApplicationController
   end
 
   private
-  def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
-  end
 
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+  # End of private methods.
 end
