0a7feed8ce3d9bf36e5e5775787acea22089e244
Dillon Downey <downeydillon@gmail.com>
2015-07-23 20:15:35 -0700

    Update db schema with boolean admin attribute

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150722015108) do
+ActiveRecord::Schema.define(version: 20150724031019) do
 
   create_table "add_index_to_users_emails", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -21,12 +21,13 @@ ActiveRecord::Schema.define(version: 20150722015108) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at",            null: false
-    t.datetime "updated_at",            null: false
+    t.datetime "created_at",                            null: false
+    t.datetime "updated_at",                            null: false
     t.string   "password_digest"
     t.string   "password"
     t.string   "password_confirmation"
     t.string   "remember_token"
+    t.boolean  "admin",                 default: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
