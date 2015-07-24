da16a01ec86040a6e4309d8e108d8f069f205bc7
Dillon Downey <downeydillon@gmail.com>
2015-07-24 10:02:50 -0700

    Describe delete links as an admin user tests.

--- spec/features/user_pages_spec.rb
+++ spec/features/user_pages_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe "User pages", type: :feature do
   let(:user) { FactoryGirl.create(:user) }
 
   describe "index" do
+
     let(:user) { FactoryGirl.create(:user) }
 
     before(:each) do
@@ -29,36 +30,28 @@ RSpec.describe "User pages", type: :feature do
         end
       end
     end
-    #
-    #
-    #
-    # Left off on delete links.... something is broken. expecting key
-    #
-    #
-    #
-    #
-    #
-    # describe "delete links" do
 
-    #   it { expect(page).to_not have_link 'delete' }
+    describe "delete links" do
 
-    #   describe "as an admin user" do
+      it { expect(page).to_not have_link('delete') }
 
-    #     let(:admin) { FactoryGirl.create(:admin) }
-    #     before do
-    #       sign_in admin
-    #       visit users_path
-    #     end
+      describe "as an admin user" do
 
-    #     it { expect(page).to have_link('delete', href: user_path(User.first)) }
+        let(:admin) { FactoryGirl.create(:admin) }
+        before do
+          sign_in admin
+          visit users_path
+        end
 
-    #     it "should be able to delete another user" do
-    #       expect { click_link('delete') }.to change(User, :count).by(-1) }
-    #     end
+        it { expect(page).to have_link('delete', href: user_path(User.first)) }
 
-    #     it { expect(page).not_to have_link('delete', href: user_path(admin)) }
-    #   end
-    # end
+        it "should be able to delete another user" do
+          expect { click_link('delete') }.to change(User, :count).by(-1)
+        end
+
+        it { expect(page).not_to have_link('delete', href: user_path(admin)) }
+      end
+    end
   end
 
   describe "signup" do
