fb033117b7593a4c46ee8f4d0b79e2faf587c596
Dillon Downey <downeydillon@gmail.com>
2015-07-25 11:46:45 -0700

    Add routes create and destroy resources for microposts.

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
+  root to: 'static_pages#home'
+
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
+  resources :microposts, only: [:create, :destroy]
+
   match '/signup', to: 'users#new', via: :get
   match '/signin', to: 'sessions#new', via: :get
   match '/signout', to: 'sessions#destroy', via: :delete
-  # Root
-  root to: 'static_pages#home'
-
-  # Static Pages Controller
   match '/help', to: 'static_pages#help', via: :get
   match '/about', to: 'static_pages#about', via: :get
   match '/contact', to: 'static_pages#contact', via: :get
