0d6ae71bc29eca3f7b4c75cecd01a1a1acd19a57
Dillon Downey <downeydillon@gmail.com>
2015-07-24 19:13:25 -0700

    Update db schema with Microposts.

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150724031019) do
+ActiveRecord::Schema.define(version: 20150725014610) do
 
   create_table "add_index_to_users_emails", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "microposts", force: :cascade do |t|
+    t.string   "content"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "microposts", ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
