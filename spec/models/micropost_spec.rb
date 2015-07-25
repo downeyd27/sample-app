f363e1f6c6abfd6add9b5cd943135deec6767770
Dillon Downey <downeydillon@gmail.com>
2015-07-24 19:14:17 -0700

    Model tests to see if Microposts respond to field content or user_id.

--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Micropost, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:user) { FactoryGirl.create(:user) }
+  before do
+    # this code is wrong
+    @micropost = Micropost.new(content: "Lorem ipsum", user_id: user.id)
+  end
+
+  subject { @micropost }
+
+  it { expect(subject).to respond_to(:content) }
+  it { expect(subject).to respond_to(:user_id) }
 end
