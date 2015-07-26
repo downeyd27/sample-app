80e46dcb818806cb31484960e80294f81a1456f1
Dillon Downey <downeydillon@gmail.com>
2015-07-25 17:16:01 -0700

    Create MicropostsController with create action and micropost_params method.

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -2,10 +2,29 @@ class MicropostsController < ApplicationController
   before_filter :signed_in_user
 
   def create
+    @micropost = current_user.microposts.build(micropost_params)
 
+    if @micropost.save
+      status = 201
+      # need to write test for flash
+      flash[:success] = "Micropost created!"
+      redirect_to root_url
+    else
+      status = 400
+      # need to write test for flash
+      flash[:error] = "Micropost NOT created!"
+      render 'static_pages/home'
+    end
   end
 
   def destroy
 
   end
+
+  private
+
+    def micropost_params
+      params.require(:micropost).permit(:content)
+    end
+  # end of private methods
 end
